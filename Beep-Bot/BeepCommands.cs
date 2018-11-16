7ee3124972b8436329de989dc73cb688f1703b06
Josiah Bradbury <herrozerro@gmail.com>
2018-11-15 19:53:47 -0600

    Initial commit for demo bot from Developer Meeting Presentation.

---
+++ Beep-Bot/BeepCommands.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+
+namespace Beep_Bot
+{
+    [Group("admin")]
+    class BeepCommands
+    {
+        [Group("secrets")]
+        class SubBeeps
+        {
+            [Command("Beep")]
+            [Description("Boops when you Beep")]
+            [Aliases("beep", "bep")]
+            public async Task BeepMessage(CommandContext ctx)
+            {
+                InteractivityModule interact = ctx.Client.GetInteractivityModule();
+
+                await ctx.RespondAsync("What kinds of Boops would you like?");
+                await ctx.RespondAsync("1: Boop");
+                await ctx.RespondAsync("2: Bop");
+
+                var response = await interact.WaitForMessageAsync(x => x.Author == ctx.Message.Author, TimeSpan.FromMinutes(1));
+
+                if (response == null)
+                {
+                    await ctx.RespondAsync("No Boops for u!");
+                }
+
+                switch (response.Message.Content)
+                {
+                    case "1":
+                        await ctx.RespondAsync("Boop");
+                        break;
+                    case "2":
+                        await ctx.RespondAsync("Bop");
+                        break;
+                    default:
+                        await ctx.RespondAsync("No");
+                        break;
+
+                }
+
+                //await ctx.RespondAsync("Boop");
+            }
+        }
+
+
+    }
+}
---
+++ Beep-Bot/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+
+namespace Beep_Bot
+{
+    class Program
+    {
+        static DiscordClient client;
+        static CommandsNextModule commands;
+        static InteractivityModule interact;
+
+        static async Task Main(string[] args)
+        {
+            client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "{Your Token Here}",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+
+            });
+
+            commands = client.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+
+            commands.RegisterCommands<BeepCommands>();
+
+            interact = client.UseInteractivity(new InteractivityConfiguration()
+            {
+
+            });
+
+            //client.MessageCreated += async e =>
+            //{
+            //    if (e.Message.Content.StartsWith("!"))
+            //    {
+            //        switch (e.Message.Content)
+            //        {
+            //            case "!Beep":
+            //                await e.Message.RespondAsync("Boop");
+            //                break;
+            //            case "!Boop":
+            //                await e.Message.RespondAsync("Bop");
+            //                break;
+            //            default:
+            //                await e.Message.RespondAsync("Paul Keep doing your homework!");
+            //                break;
+            //        }
+            //    }
+            //};
+
+            await client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        
+    }
+}
